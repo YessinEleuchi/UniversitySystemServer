ca2d15b44c284521042af3cb4a70a8df8406fb50
YessinEleuchi <yessineleuchi@iit.ens.tn>
2025-12-27 15:12:01 +0100

    clean imports  main file

--- src/main/java/com/eduflow/UniFlowApplication.java
+++ src/main/java/com/eduflow/UniFlowApplication.java
@@ -1,18 +1,7 @@
 package com.eduflow;
 
-import com.eduflow.academic.domain.Cycle;
-import com.eduflow.academic.domain.enums.ProgramCycle;
-import com.eduflow.academic.repo.CycleRepository;
-import com.eduflow.people.domain.Role;
-import com.eduflow.people.domain.UserAccount;
-import com.eduflow.people.repo.UserAccountRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Set;
 
 @SpringBootApplication
 public class UniFlowApplication {
