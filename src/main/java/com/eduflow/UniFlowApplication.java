0bfc0faf2335689eca056b6f86d97740d7068a47
YessinEleuchi <yessineleuchi@iit.ens.tn>
2025-12-27 15:10:17 +0100

    clean main file

--- src/main/java/com/eduflow/UniFlowApplication.java
+++ src/main/java/com/eduflow/UniFlowApplication.java
@@ -3,30 +3,20 @@ package com.eduflow;
 import com.eduflow.academic.domain.Cycle;
 import com.eduflow.academic.domain.enums.ProgramCycle;
 import com.eduflow.academic.repo.CycleRepository;
+import com.eduflow.people.domain.Role;
+import com.eduflow.people.domain.UserAccount;
+import com.eduflow.people.repo.UserAccountRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @SpringBootApplication
 public class UniFlowApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UniFlowApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner initCycles(CycleRepository repo) {
-        return args -> {
-            if (repo.count() == 0) {   // avoid duplicate on restart because of unique code
-                Cycle c = Cycle.builder()
-                        .code("CYC-001")
-                        .label(ProgramCycle.LICENCE)   // use one value from your enum
-                        .build();
-
-                repo.save(c);
-                System.out.println("✅ Saved Cycle with id = " + c.getId());
-            }
-        };
     }
 }
