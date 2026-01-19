f6def20f6da4dfd842cf40768edb50f1350a5f99
YessinEleuchi <yessineleuchi@iit.ens.tn>
2026-01-19 02:29:13 +0100

    replace admin auth controller

--- src/main/java/com/eduflow/security/presentation/Controller/AdminAuthController.java
+++ src/main/java/com/eduflow/security/presentation/Controller/AdminAuthController.java
@@ -1,4 +1,4 @@
-package com.eduflow.security.presentation;
+package com.eduflow.security.presentation.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
